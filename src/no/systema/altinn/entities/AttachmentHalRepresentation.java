c18d182df319e637e89a189a4dc5a376e88d9dd6
Fredrik Möller <fredrik@systema.no>
2018-03-07 12:30:49 +0100

    misc fixes

---
+++ src/no/systema/altinn/entities/AttachmentHalRepresentation.java
@@ -0,0 +1,34 @@
+package no.systema.altinn.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import de.otto.edison.hal.HalRepresentation;
+
+public class AttachmentHalRepresentation extends HalRepresentation {
+
+    @JsonProperty("FileName")
+	private String fileName;
+//	FileName	String	Filename of the Attachment.
+//	Data	Byte[]	The Attachment data (base64 encoded).
+    @JsonProperty("AttachmentLink")
+    private String attachmentLink;
+//	AttachmentLink	Uri	Download link for the Attachment.
+//	AttachmentType	String	Attachment type. This is currently unused, but will be used in a future release.
+//	Encrypted	Boolean	Flag indicating if this is an encrypted Attachment.
+//	SigningLocked	Boolean	Value indicating whether the user should be allowed to change signing selection of the attachment. If both SigningLocked and SingedByDefault are true, then signing is required.
+//	SignedByDefault	Boolean	Value indicating whether the service owner wants the attachment to be signed. If both SigningLocked and SingedByDefault are true, then signing is required.	
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getAttachmentLink() {
+		return attachmentLink;
+	}
+	public void setAttachmentLink(String attachmentLink) {
+		this.attachmentLink = attachmentLink;
+	}
+
+    
+}
